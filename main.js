daba3ce8e27940bc61d7ccdc4bdb55e6ffd46f11
Noam <noamsoudai@gmail.com>
2021-12-07 17:43:35 +0200

    nothing

--- main.js
+++ main.js
@@ -2,9 +2,27 @@ const cards = [
   { name: "A" },
   { name: "B" },
   { name: "C" },
+  { name: "D" },
+  { name: "E" },
+  { name: "F" },
+  { name: "G" },
+  { name: "H" },
+  { name: "I" },
+  { name: "J" },
+  { name: "K" },
+  { name: "L" },
   { name: "A" },
   { name: "B" },
   { name: "C" },
+  { name: "D" },
+  { name: "E" },
+  { name: "F" },
+  { name: "G" },
+  { name: "H" },
+  { name: "I" },
+  { name: "J" },
+  { name: "K" },
+  { name: "L" },
 ];
 
 function randomRange(a, b) {
@@ -29,11 +47,47 @@ function shuffle(arr) {
   }
 }
 
-function main() {
-  console.log([...cards]);
-  shuffle(cards);
-  console.log(cards);
+let arrCards = [];
+
+const cardClicked = (evn) => {
+  // console.log(evn.target.innerHTML);
+  const cardE1 = evn.target;
+  const idx = cardE1.id;
+  cardE1.innerHTML = cards[idx].name;
+  arrCards.push(cards[idx]);
+  console.log(arrCards);
+  if (arrCards === 2) {
+  }
+};
+
+function createCardElement(idx) {
+  const board = document.getElementById("board");
+  const cardE1 = document.createElement("div");
+  //<div></div>
+  // cardE1.innerHTML = card.name;
+  cardE1.id = idx;
+  //<div></div>
+  cardE1.className = "card";
+  //div class = 'card'
+  // cardE1.addEventListener("click", f);
+  cardE1.onclick = cardClicked;
+
+  board.appendChild(cardE1);
+  //<div id = board>
+  //<div class = 'card' >C></div>
+  // </div>
 }
+
+function main() {
+  // console.log([...cards]);
+  shuffle(cards);
+  for (let idx in cards) {
+    createCardElement(idx);
+  }
+  // console.log(cards);
+}
+
+main();
 
 function match(i, j) {
   return i != j && arr[i].name == arr[j].name;
@@ -46,6 +100,8 @@ function deleteC(i, j) {
   }
 }
 
-main();
-
 deleteC(0, 2);
+
+window.onload = () => {
+  // main();
+};
